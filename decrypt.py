b87cce8ae1b078b6607a7b68318a569765ddae6b
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-06 15:48:18 +0700

    Create decrypt.py

---
+++ decrypt.py
@@ -0,0 +1,19 @@
+def caesar_cipher_decrypt(text, shift):
+    if shift >= 26:
+        raise TypeError("Shift cannot more than 25")
+
+    alphabets = [string.ascii_lowercase, string.ascii_uppercase, string.punctuation]
+
+    shift = 26-shift
+
+    def shift_alphabet(alphabets):
+        return alphabets[shift:] + alphabets[:shift]
+
+    
+    shifted_alphabets = tuple(map(shift_alphabet, alphabets))
+    final_alphabet = ''.join(alphabets)
+    final_shifted_alphabet = ''.join(shifted_alphabets)
+
+    table = str.maketrans(final_alphabet, final_shifted_alphabet)
+
+    return text.translate(table)
